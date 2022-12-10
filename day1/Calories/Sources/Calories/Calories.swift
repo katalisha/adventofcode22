f5da7d56279767c6cf51efd9b655c1d4473e3f9d
Kate <kate@Kates-MacBook-Pro-2.local>
2022-12-10 14:42:09 +1100

    a little more refactoring of day 1

--- day1/Calories/Sources/Calories/Calories.swift
+++ day1/Calories/Sources/Calories/Calories.swift
@@ -3,15 +3,21 @@ import Foundation
 public func topElves(_ elvesToReturn: Int) -> Int {
     let file = try! readFile()
     
-    
     let result = file.components(separatedBy: "\n\n")
         .map(parseBlockToArray)
         .sorted()
-        .reversed()
+        .reversed() // this is more readable than custom sort
         .prefix(through: elvesToReturn - 1)
-        .reduce(Int(0), +)
+        .reduce(0, +)
     
     return result
+}
+
+func parseBlockToArray(block: String) -> Int {
+    return block
+        .components(separatedBy: "\n")
+        .map {Int($0) ?? 0}
+        .reduce(0, +)
 }
 
 func readFile() throws -> String {
@@ -19,12 +25,3 @@ func readFile() throws -> String {
     let data = try String(contentsOf: path!)
     return data
 }
-
-func parseBlockToArray(block: String) -> Int {
-    return block
-        .components(separatedBy: "\n")
-        .map {Int($0)}  // parse as int
-        .compactMap{$0} // remove nils
-        .reduce(0, +)   // sum
-}
-
